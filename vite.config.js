9cc6c63c85cabf1b23b8fdbb8ed2ea11557d7fac
KHab1bull0 <habibullotoyrilmasov@gmail.com>
2025-05-13 21:48:32 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // yoki kerakli subpath
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, // dev server uchun
+  }
 })
