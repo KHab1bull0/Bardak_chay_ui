6526e9eb1fd4f2fce9d57cc002e35af068923a00
KHab1bull0 <habibullotoyrilmasov@gmail.com>
2025-05-30 11:08:08 +0500

    updated url

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-const API_URI = 'http://localhost:8080/'
-// const API_URI = 'https://bardak.mohirsoft.uz/'
+// const API_URI = 'http://localhost:8080/'
+const API_URI = 'https://bardak.mohirsoft.uz/'
 
 export { API_URI };
